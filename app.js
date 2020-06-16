c37f6358fa5478a59ded3cd58f7d65b3327b2003
code-with-garvit <63104084+code-with-garvit@users.noreply.github.com>
2020-06-17 01:10:00 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+$('.marker').bind('click', function() {
+  $('.card').addClass('active');
+  $('.marker').addClass('inactive');
+});
+
+$('.card').bind('click', function() {
+  $('.card').removeClass('active');
+  $('.marker').removeClass('inactive');
+});
+
